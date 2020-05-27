61a49269318144aa096bc7c07fa1d905bd72a010
riship99 <40177825+riship99@users.noreply.github.com>
2020-05-27 14:54:32 +0530

    Test code pushed to repository.

---
+++ Its result time/Main.java
@@ -0,0 +1,16 @@
+#include<iostream>
+int main(){
+int id1=132575;
+  int marks1=98;
+  int id2=132574;
+  int marks2=85;
+  std::cin>>id1;
+  std::cin>>marks1;
+    std::cin>>id2;
+  std::cin>>marks2;
+  std::cout<<"Id : "<<id1;
+  std::cout<<"\nTotalmarks : "<<marks1;
+  //std::cout<<"\nId : "<<id2;
+  //std::cout<<"\nTotalmarks : "<<marks2;
+
+}
